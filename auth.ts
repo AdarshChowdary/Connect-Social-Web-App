f13425ef01b91833898d29b822699ab207756e3e
Adarh16 <aaadarsh16@gmail.com>
2024-10-23 21:03:46 +0530

    Updated auth.ts

--- auth.ts
+++ auth.ts
@@ -33,11 +33,11 @@ declare module "lucia" {
 }
 
 interface DatabaseUserAttributes {
-  id: String;
-  username: String;
-  displayName: String;
-  avatarUrl: String | null;
-  googleId: String | null;
+  id: string;
+  username: string;
+  displayName: string;
+  avatarUrl: string | null;
+  googleId: string | null;
 }
 
 export const google = new Google(
